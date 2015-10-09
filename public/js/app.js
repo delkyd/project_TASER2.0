e74df0197ede035196ac283cac49fba9b3a11f3d
nmangubat <aeon37@gmail.com>
2015-10-09 10:14:50 -0700

    final clean up

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,47 @@
-function init() {
-	gapi.client.setApiKey('AIzaSyBLT46OdmqPHt6lweLKQ4VtddKUsEFjvK4');
-	gapi.client.load("youtube", "v3", function() {
 
-	});
+
+//YOUTUBE AUTHORIZATION
+function init( ) {
+	gapi.client.setApiKey( 'AIzaSyBLT46OdmqPHt6lweLKQ4VtddKUsEFjvK4' ); //sets up key for authorization
+	gapi.client.load( "youtube", "v3", function( ) { //loads API
+
+	} );
 }
 
+//PULLING FROM THE SEARCH FUNCTION
+$( function( ) {
+	$( "form" ).on( "submit" , function( e ) { //taking submission from search bar
+		e.preventDefault( ); //preventing from sending too quickly
+		console.log( "got here 1" )		
 
-$(function() {
-	$("form").on("submit", function(e) {
-		e.preventDefault();
-		console.log("got here 1")		
-
-		var request = gapi.client.youtube.search.list({
+		//SORTING OUT RESULTS
+		var request = gapi.client.youtube.search.list( { //defining parameters
 			part: "snippet",
 			type: "video",
-			q: encodeURIComponent( $("#response").val() ).replace(/%20/g, "+"),
-			maxResults: 1,
-			orderBy: "relevance",
-			publishedAfter: "2015-01-01T00:00:00Z",
-		});
-		request.execute(function(response) {
-			//console.log(response)
+			q: encodeURIComponent( $( "#response" ).val( ) ).replace( /%20/g, "+" ), //defines search term by response
+			maxResults: 1, //number of results 
+			orderBy: "relevance", //how video will be organized
+			publishedAfter: "2015-01-01T00:00:00Z", //keeping videos current
+		} );
+
+		//EXECUTES SEARCH
+		request.execute( function( response ) {
 			var results = response.result;
-			$.each(results['items'], function(index, item) {
-				// var test= items[0]['id'].videoId
-				var test = results['items'][0].id.videoId
-				console.log(test)
-				$('#results').append('<iframe class="video w100 animated fadeIn" width=640 height=360 src="https://www.youtube.com/embed/'+test+ '?autoplay=1" frameborder="0" ></iframe>')
+			$.each( results[ 'items' ], function( index, item ) { //how jQUERY should sort through said results
+				var test = results[ 'items' ][ 0 ].id.videoId //chooses first video based on relevance
+				console.log( test )
+				$( '#results' ).append( '<iframe class="video w100 animated fadeIn" width=640 height=360 src="https://www.youtube.com/embed/'+test+ '?autoplay=1" frameborder="0" ></iframe>' ) //defines iframe size and source
 
-			});
+			} );
 
-		});
-		lights()
-	});
+		} );
+		lights( ) //executes lights function at the end of youtube search results
+	} );
 
-});
+} );
 
-function resetVideoHeight() {
-    $(".video").css("height", $("#results").width() * 9/16);
+//DEFINES SYTLING OF VIDEO
+function resetVideoHeight( ) {
+    $( ".video" ).css( "height", $( "#results" ).width( ) * 9/16 );
 }
 
